2690dd4fb8817a0661d978e87ca6752c507fc6d7
godcong <jumbycc@163.com>
2018-03-20 18:03:32 +0800

    undone: a new version from wechat api

--- official_account/qrcode.go
+++ official_account/qrcode.go
@@ -1,4 +1,19 @@
 package official_account
 
+import "github.com/godcong/wego/core"
+
 type QrCode struct {
+	core.Config
+	*OfficialAccount
+}
+
+//http请求方式: POST
+//URL: https://api.weixin.qq.com/cgi-bin/qrcode/create?access_token=TOKEN
+//POST数据格式：json
+//POST数据例子：{"expire_seconds": 604800, "action_name": "QR_SCENE", "action_info": {"scene": {"scene_id": 123}}}
+//
+//或者也可以使用以下POST数据创建字符串形式的二维码参数：
+//{"expire_seconds": 604800, "action_name": "QR_STR_SCENE", "action_info": {"scene": {"scene_str": "test"}}}
+func (q *QrCode) Create() {
+
 }
