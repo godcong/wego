f168109a8db56647fa31a3eb1f1e28613c412621
godcong <jumbycc@163.com>
2018-03-21 14:33:29 +0800

    undone: a new version from wechat api

--- core/message.go
+++ core/message.go
@@ -8,75 +8,77 @@ type Message struct {
 	message.Message
 	/*message*/
 	Content      CDATA
-	PicUrl       CDATA //图片链接（由系统生成）
-	MediaId      CDATA //图片消息媒体id，可以调用多媒体文件下载接口拉取数据。
-	Title        CDATA //标题
-	AppId        CDATA //小程序appid
-	PagePath     CDATA //小程序页面路径
-	ThumbUrl     CDATA //封面图片的临时cdn链接
-	ThumbMediaId CDATA //封面图片的临时素材id
+	PicUrl       CDATA // 图片链接（由系统生成）
+	MediaId      CDATA // 图片消息媒体id，可以调用多媒体文件下载接口拉取数据。
+	Title        CDATA // 标题
+	AppId        CDATA // 小程序appid
+	PagePath     CDATA // 小程序页面路径
+	ThumbUrl     CDATA // 封面图片的临时cdn链接
+	ThumbMediaId CDATA // 封面图片的临时素材id
 	Items        []*message.NewItem
-	Format       CDATA //语音格式，如amr，speex等
-	Recognition  CDATA //语音识别结果，UTF8编码
+	Format       CDATA // 语音格式，如amr，speex等
+	Recognition  CDATA // 语音识别结果，UTF8编码
 	Location_X   float64
 	Location_Y   float64
 	Scale        int64
 	Label        CDATA
-	Description  CDATA //消息描述
+	Description  CDATA // 消息描述
 	Url          CDATA
 	/*event*/
 	message.Event
-	EventKey  CDATA   //事件KEY值，qrscene_为前缀，后面为二维码的参数值
-	Ticket    CDATA   //二维码的ticket，可用来换取二维码图片
-	Latitude  float64 //地理位置纬度
-	Longitude float64 //地理位置经度
-	Precision float64 //地理位置精度
+	EventKey  CDATA   // 事件KEY值，qrscene_为前缀，后面为二维码的参数值
+	Ticket    CDATA   // 二维码的ticket，可用来换取二维码图片
+	Latitude  float64 // 地理位置纬度
+	Longitude float64 // 地理位置经度
+	Precision float64 // 地理位置精度
 
-	MenuID CDATA //指菜单ID，如果是个性化菜单，则可以通过这个字段，知道是哪个规则的菜单被点击了。
+	MenuID CDATA // 指菜单ID，如果是个性化菜单，则可以通过这个字段，知道是哪个规则的菜单被点击了。
 
-	ScanCodeInfo     message.ScanCodeInfo     //扫描信息
-	SendPicsInfo     message.SendPicsInfo     //发送的图片信息
-	SendLocationInfo message.SendLocationInfo //发送的位置信息
+	ScanCodeInfo     message.ScanCodeInfo     // 扫描信息
+	SendPicsInfo     message.SendPicsInfo     // 发送的图片信息
+	SendLocationInfo message.SendLocationInfo // 发送的位置信息
 
-	Status CDATA //	发送状态为成功
-	//TransInfo        message.TransInfo        //
-	//attributes   Map
-	//properties   []string
-	//aliases      Map
+	Status CDATA // 	发送状态为成功
+
+	ExpiredTime int64 // 有效期 (整形)，指的是时间戳，将于该时间戳认证过期
+	FailTime    int64 // 失败发生时间 (整形)，时间戳
+	FailReason  CDATA // 认证失败的原因
+	// 名称认证成功（即命名成功）
+
 }
 
-//type Article struct {
-//}
+// type Article struct {
+// }
 //
-//func NewMessage() *Message {
-//	return &Message{}
-//}
+// func NewMessage() *Message {
+// 	return &Message{}
+// }
 
 //
-//func (m *Message) SetAttribute(key string, val interface{}) *Message {
-//	m.properties = append(m.properties, key)
-//	m.attributes.Set(key, val)
-//	return m
-//}
+// func (m *Message) SetAttribute(key string, val interface{}) *Message {
+// 	m.properties = append(m.properties, key)
+// 	m.attributes.Set(key, val)
+// 	return m
+// }
 //
-//func (m *Message) SetAttributes(m0 Map) *Message {
-//	for k, v := range m0 {
-//		m.SetAttribute(k, v)
-//	}
-//	return m
-//}
+// func (m *Message) SetAttributes(m0 Map) *Message {
+// 	for k, v := range m0 {
+// 		m.SetAttribute(k, v)
+// 	}
+// 	return m
+// }
 //
-//func (m *Message) GetAttribute(key string) interface{} {
-//	return m.attributes.Get(key)
-//}
+// func (m *Message) GetAttribute(key string) interface{} {
+// 	return m.attributes.Get(key)
+// }
 //
-//func (m *Message) GetAttributes(keys []string) []interface{} {
-//	var m0 []interface{}
-//	for _, v := range keys {
-//		m0 = append(m0, m.attributes.Get(v))
-//	}
-//	return m0
-//}
+// func (m *Message) GetAttributes(keys []string) []interface{} {
+// 	var m0 []interface{}
+// 	for _, v := range keys {
+// 		m0 = append(m0, m.attributes.Get(v))
+// 	}
+// 	return m0
+// }
 
 func (m *Message) SetType(msgType message.MsgType) *Message {
 	m.MsgType = message.MSGCDATA{MsgType: msgType}
--- core/message/event.go
+++ core/message/event.go
@@ -5,20 +5,27 @@ import "strings"
 type EvtType string
 
 const (
-	EventSubscribe             EvtType = "subscribe"              //订阅
-	EventUnsubscribe           EvtType = "unsubscribe"            //取消订阅
-	EventScan                  EvtType = "scan"                   //用户已经关注公众号，则微信会将带场景值扫描事件推送给开发者
-	EventLocation              EvtType = "location"               //上报地理位置事件
-	EventClick                 EvtType = "click"                  //点击菜单拉取消息时的事件推送
-	EventView                  EvtType = "view"                   //点击菜单跳转链接时的事件推送
-	EventScancodePush          EvtType = "scancode_push"          //扫码推事件的事件推送
-	EventScancodeWaitmsg       EvtType = "scancode_waitmsg"       //扫码推事件且弹出“消息接收中”提示框的事件推送
-	EventPicSysphoto           EvtType = "pic_sysphoto"           //弹出系统拍照发图的事件推送
-	EventPicPhotoOrAlbum       EvtType = "pic_photo_or_album"     //弹出拍照或者相册发图的事件推送
-	EventPicWeixin             EvtType = "pic_weixin"             //弹出微信相册发图器的事件推送
-	EventLocationSelect        EvtType = "location_select"        //弹出地理位置选择器的事件推送
-	EventTemplatesendjobfinish EvtType = "templatesendjobfinish"  //发送模板消息推送通知
-	EventUserEnterTempsession  EvtType = "user_enter_tempsession" //会话事件
+	EventSubscribe                  EvtType = "subscribe"                    // 订阅
+	EventUnsubscribe                EvtType = "unsubscribe"                  // 取消订阅
+	EventScan                       EvtType = "scan"                         // 用户已经关注公众号，则微信会将带场景值扫描事件推送给开发者
+	EventLocation                   EvtType = "location"                     // 上报地理位置事件
+	EventClick                      EvtType = "click"                        // 点击菜单拉取消息时的事件推送
+	EventView                       EvtType = "view"                         // 点击菜单跳转链接时的事件推送
+	EventScancodePush               EvtType = "scancode_push"                // 扫码推事件的事件推送
+	EventScancodeWaitmsg            EvtType = "scancode_waitmsg"             // 扫码推事件且弹出“消息接收中”提示框的事件推送
+	EventPicSysphoto                EvtType = "pic_sysphoto"                 // 弹出系统拍照发图的事件推送
+	EventPicPhotoOrAlbum            EvtType = "pic_photo_or_album"           // 弹出拍照或者相册发图的事件推送
+	EventPicWeixin                  EvtType = "pic_weixin"                   // 弹出微信相册发图器的事件推送
+	EventLocationSelect             EvtType = "location_select"              // 弹出地理位置选择器的事件推送
+	EventTemplatesendjobfinish      EvtType = "templatesendjobfinish"        // 发送模板消息推送通知
+	EventUserEnterTempsession       EvtType = "user_enter_tempsession"       // 会话事件
+	EventQualificationVerifySuccess EvtType = "qualification_verify_success" // 资质认证成功（此时立即获得接口权限）
+	EventQualificationVerifyFail    EvtType = "qualification_verify_fail"    // 资质认证失败
+	EventNamingVerifySuccess        EvtType = "naming_verify_success"        // 名称认证成功（即命名成功）
+	EventNamingVerifyFail           EvtType = "naming_verify_fail"           // 名称认证失败（这时虽然客户端不打勾，但仍有接口权限）
+	EventAnnualRenew                EvtType = "annual_renew"                 // 年审通知
+	EventVerifyExpired              EvtType = "verify_expired"               // 认证过期失效通知审通知
+
 )
 
 type EVTCDATA struct {
