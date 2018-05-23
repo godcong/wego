4aaba5ac699df561caede1031899633c8d891fb2
jumbycc@163.com <godcong>
2018-05-24 00:44:53 +0800

    fix some bug in use

--- app/official_account/template.go
+++ app/official_account/template.go
@@ -74,11 +74,11 @@ func NewTemplate() *Template {
 //失败：
 //{"errcode":43100,"errmsg":"change template too frequently hint: [ZJ3zDA0168vr23]"}
 func (t *Template) SetIndustry(id1, id2 string) *net.Response {
-	token := t.token.GetToken()
 	resp := t.client.HttpPostJson(
 		t.client.Link(API_SET_INDUSTRY_URL_SUFFIX),
+		t.token.GetToken().KeyMap(),
 		util.Map{"industry_id1": id1, "industry_id2": id2},
-		util.Map{net.REQUEST_TYPE_QUERY.String(): token.KeyMap()})
+	)
 	return resp
 
 }
@@ -86,10 +86,9 @@ func (t *Template) SetIndustry(id1, id2 string) *net.Response {
 //成功：
 //{"primary_industry":{"first_class":"IT科技","second_class":"互联网|电子商务"},"secondary_industry":{"first_class":"IT科技","second_class":"IT软件与服务"}}
 func (t *Template) GetIndustry() *net.Response {
-	token := t.token.GetToken()
 	resp := t.client.HttpGet(
 		t.client.Link(GET_INDUSTRY_URL_SUFFIX),
-		util.Map{net.REQUEST_TYPE_QUERY.String(): token.KeyMap()})
+		t.token.GetToken().KeyMap())
 	return resp
 }
 
@@ -99,11 +98,10 @@ func (t *Template) GetIndustry() *net.Response {
 // 成功：
 // {"errcode":0,"errmsg":"ok","template_id":"tAsZKUQO0zNkrfvsTi2JexHJ9ZPudXuZSdcurGzE7Yo"}
 func (t *Template) Add(shortId string) *net.Response {
-	token := t.token.GetToken()
 	resp := t.client.HttpPostJson(
 		t.client.Link(API_ADD_TEMPLATE_URL_SUFFIX),
-		util.Map{"template_id_short": shortId},
-		util.Map{net.REQUEST_TYPE_QUERY.String(): token.KeyMap()})
+		t.token.GetToken().KeyMap(),
+		util.Map{"template_id_short": shortId})
 	return resp
 }
 
@@ -113,10 +111,8 @@ func (t *Template) Add(shortId string) *net.Response {
 //成功:
 //{"errcode":0,"errmsg":"ok","msgid":191569096301903872}
 func (t *Template) Send(template *message.Template) *net.Response {
-
 	resp := t.client.HttpPostJson(
 		t.client.Link(MESSAGE_TEMPLATE_SEND_URL_SUFFIX),
-
 		t.token.GetToken().KeyMap(),
 		template,
 	)
@@ -127,10 +123,9 @@ func (t *Template) Send(template *message.Template) *net.Response {
 //成功:
 //{"template_list":[{"template_id":"tAsZKUQO0zNkrfvsTi2JexHJ9ZPudXuZSdcurGzE7Yo","title":"订单支付成功","primary_industry":"IT科技","deputy_industry":"互联网|电子商务","content":"{{first.DATA}}\n\n支付金额：{{orderMoneySum.DATA}}\n商品信息：{{orderProductName.DATA}}\n{{Remark.DATA}}","example":"我们已收到您的货款，开始为您打包商品，请耐心等待: )\n支付金额：30.00元\n商品信息：我是商品名字\n\n如有问题请致电400-828-1878或直接在微信留言，小易将第一时间为您服务！"},{"template_id":"sBMv7KrI5O66W-lqMQXKMVAs6sdtk0IKa7P1IoqC_mg","title":"订单支付成功","primary_industry":"IT科技","deputy_industry":"互联网|电子商务","content":"{{first.DATA}}\n\n支付金额：{{orderMoneySum.DATA}}\n商品信息：{{orderProductName.DATA}}\n{{Remark.DATA}}","example":"我们已收到您的货款，开始为您打包商品，请耐心等待: )\n支付金额：30.00元\n商品信息：我是商品名字\n\n如有问题请致电400-828-1878或直接在微信留言，小易将第一时间为您服务！"},{"template_id":"mO3VehTDPKVl-bJ1-58ZmfeFTuKwWP9yg6_tzkt1Ab0","title":"订阅模板消息","primary_industry":"","deputy_industry":"","content":"{{content.DATA}}","example":""},{"template_id":"awT3aSQJdtWqn7VRUNLzdEboGb1fONot3Z7SrsBtsjg","title":"订单支付成功","primary_industry":"IT科技","deputy_industry":"互联网|电子商务","content":"{{first.DATA}}\n\n支付金额：{{orderMoneySum.DATA}}\n商品信息：{{orderProductName.DATA}}\n{{Remark.DATA}}","example":"我们已收到您的货款，开始为您打包商品，请耐心等待: )\n支付金额：30.00元\n商品信息：我是商品名字\n\n如有问题请致电400-828-1878或直接在微信留言，小易将第一时间为您服务！"},{"template_id":"vc2ekfQmEP9qE9eBW9gGWaUrsLvztC9XOeB-cftLroo","title":"订单支付成功","primary_industry":"IT科技","deputy_industry":"互联网|电子商务","content":"{{first.DATA}}\n\n支付金额：{{orderMoneySum.DATA}}\n商品信息：{{orderProductName.DATA}}\n{{Remark.DATA}}","example":"我们已收到您的货款，开始为您打包商品，请耐心等待: )\n支付金额：30.00元\n商品信息：我是商品名字\n\n如有问题请致电400-828-1878或直接在微信留言，小易将第一时间为您服务！"}]}
 func (t *Template) GetAllPrivate() *net.Response {
-	token := t.token.GetToken()
 	resp := t.client.HttpGet(
 		t.client.Link(GET_ALL_PRIVATE_TEMPLATE_URL_SUFFIX),
-		util.Map{net.REQUEST_TYPE_QUERY.String(): token.KeyMap()},
+		t.token.GetToken().KeyMap(),
 	)
 	return resp
 }
@@ -139,11 +134,10 @@ func (t *Template) GetAllPrivate() *net.Response {
 //成功:
 //{"errcode":0,"errmsg":"ok"}
 func (t *Template) DelAllPrivate(templateID string) *net.Response {
-	token := t.token.GetToken()
 	resp := t.client.HttpPostJson(
 		t.client.Link(DEL_PRIVATE_TEMPLATE_URL_SUFFIX),
+		t.token.GetToken().KeyMap(),
 		util.Map{"template_id": templateID},
-		util.Map{net.REQUEST_TYPE_QUERY.String(): token.KeyMap()},
 	)
 	return resp
 
