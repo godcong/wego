03d063849d9d8a56147feabd4873decb57653e7b
godcong <jumbycc@163.com>
2018-10-18 17:55:36 +0800

    update config

--- core/config.go
+++ core/config.go
@@ -14,9 +14,11 @@ type Tree toml.Tree
 
 //Config Application config interface */
 type Config interface {
-	Get(s string) string
-	GetD(s, d string) string
-	Set(k, v string) *Tree
+	Get(s string) interface{}
+	GetD(s string, d interface{}) interface{}
+	Set(k string, v interface{}) *Tree
+	GetString(k string) string
+	GetStringD(k, d string) string
 	GetBool(s string) bool
 	GetBoolD(s string, d bool) bool
 	GetInt(s string) int64
@@ -49,11 +51,26 @@ func (t *Tree) GetTree(s string) interface{} {
 	if t == nil {
 		return nil
 	}
+
 	return (*toml.Tree)(t).Get(s)
 }
 
-/*Get get string with out default value */
-func (t *Tree) Get(s string) string {
+/*Get get an interface from config */
+func (t *Tree) Get(s string) interface{} {
+	return t.GetTree(s)
+}
+
+/*GetD get interface with default value */
+func (t *Tree) GetD(s string, d interface{}) interface{} {
+	v := t.GetTree(s)
+	if v == nil {
+		return d
+	}
+	return v
+}
+
+/*GetString get string with out default value */
+func (t *Tree) GetString(s string) string {
 	v := t.GetTree(s)
 	if v, b := v.(string); b {
 		return v
@@ -61,18 +78,18 @@ func (t *Tree) Get(s string) string {
 	return ""
 }
 
-/*GetD get string with default value */
-func (t *Tree) GetD(s, d string) string {
-	if v := t.Get(s); v != "" {
+/*GetString get string with out default value */
+func (t *Tree) GetStringD(s, d string) string {
+	v := t.GetTree(s)
+	if v, b := v.(string); b {
 		return v
 	}
 	return d
 }
 
-/*Set set string value */
-func (t *Tree) Set(k, v string) *Tree {
-	tt := (*toml.Tree)(t)
-	tt.Set(k, v)
+/*Set set value */
+func (t *Tree) Set(k string, v interface{}) *Tree {
+	(*toml.Tree)(t).Set(k, v)
 	return t
 }
 
@@ -114,4 +131,17 @@ func (t *Tree) GetIntD(s string, d int64) int64 {
 		return d
 	}
 	return v0
+}
+
+func (t *Tree) Has(key string) bool {
+	if t == nil {
+		return false
+	}
+
+	return (*toml.Tree)(t).Has(key)
+}
+
+//NewConfig create a null config
+func NewConfig() Config {
+	return &Tree{}
 }
