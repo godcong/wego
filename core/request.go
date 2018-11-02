3ff890dd368490893561747ff81527561e9cd29b
godcong <jumbycc@163.com>
2018-11-02 18:03:02 +0800

    fix delete unused comment

--- core/request.go
+++ core/request.go
@@ -6,109 +6,6 @@ import (
 	"net/http"
 )
 
-//
-//import (
-//	"crypto/tls"
-//
-//	"net/http"
-//
-//	"bytes"
-//
-//	"crypto/x509"
-//	"errors"
-//	"io/ioutil"
-//	"log"
-//	"time"
-//
-//	"github.com/godcong/wopay/tool"
-//)
-//
-//type PayRequest struct {
-//	config PayConfig
-//}
-//
-//var (
-//	ErrorNilDomain       = errors.New("PayConfig.PayDomain().getDomain() is empty or null")
-//	ErrorLoadX509KeyPair = errors.New("LoadX509KeyPair() is empty to load")
-//	ErrorReadRootCAFile  = errors.New("read rootca.pem file error")
-//)
-//
-//func NewPayRequest(config PayConfig) *PayRequest {
-//	return &PayRequest{config: config}
-//}
-
-/*RequestType RequestType */
-//type RequestType string
-
-/*request types */
-//const (
-//RequestTypeJSON       RequestType = "json"
-//RequestTypeQuery      RequestType = "query"
-//RequestTypeXML        RequestType = "xml"
-//RequestTypeFormParams RequestType = "form_params"
-//RequestTypeFile       RequestType = "file"
-//RequestTypeMultipart  RequestType = "multipart"
-//RequestTypeString     RequestType = "string"
-//RequestTypeHeaders    RequestType = "headers"
-//RequestTypeCustom     RequestType = "custom"
-//)
-
-///*Request Request */
-//type Request struct {
-//	requestType RequestType
-//	//requestData *RequestData
-//	httpRequest *http.Request
-//	custom      func(*Request) string
-//	error error
-//}
-
-/*NewRequest NewRequest */
-//func NewRequest() *Request {
-//	r := Request{
-//		httpRequest: nil,
-//		error:       nil,
-//		//requestData: &RequestData{
-//		//	Query:  "",
-//		//	Body:   nil,
-//		//	Method: "",
-//		//	Header: http.Header{},
-//		//},
-//	}
-//	return &r
-//}
-//
-///*SetCustomCallback SetCustomCallback*/
-//func (r *Request) SetCustomCallback(f func(*Request) string) *Request {
-//	r.custom = f
-//	return r
-//}
-//
-///*GetRequestType GetRequestType */
-//func (r *Request) GetRequestType() RequestType {
-//	return r.requestType
-//}
-//
-//func (r *Request) Error() error {
-//	return r.error
-//}
-//
-///*NewRequestData NewRequestData */
-//func NewRequestData() *RequestData {
-//	data := &RequestData{
-//		Query:  "",
-//		Body:   nil,
-//		Method: "",
-//		Header: http.Header{},
-//	}
-//	//data.Header = cloneHeader(r.requestData.Header)
-//	return data
-//}
-
-/*httpRequest get http request*/
-//func (r *Request) httpRequest() *http.Request {
-//	return r.httpRequest
-//}
-
 func connectQuery(url, query string) string {
 	if query == "" {
 		return url
@@ -121,11 +18,6 @@ func parseBody(body string) io.Reader {
 		return nil
 	}
 	return bytes.NewBufferString(body)
-}
-
-/*UseUTF8 return true */
-func UseUTF8() bool {
-	return true
 }
 
 func cloneHeader(h http.Header) http.Header {
