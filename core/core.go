0758c3f9a4ce315d036d84618ead7f463aa5436d
godcong <jumbycc@163.com>
2018-11-02 18:04:08 +0800

    fix delete unused comment

--- core/core.go
+++ core/core.go
@@ -1,18 +1,3 @@
 package core
 
-var sandbox = false
-
-//SandBoxOn open sandbox
-func SandBoxOn() {
-	sandbox = true
-}
-
-//SandBoxOff close sandbox
-func SandBoxOff() {
-	sandbox = false
-}
-
-//UseSandBox check sandbox
-func UseSandBox() bool {
-	return sandbox
-}
+//TODO
