f7359900c88e9c3f45d5ff59705b743734d61b91
godcong <jumbycc@163.com>
2018-11-13 09:09:36 +0800

    update comment

--- app/official/material.go
+++ app/official/material.go
@@ -40,7 +40,6 @@ func (m *Material) AddNews(maps util.Map) core.Response {
 //AddMaterial 新增其他类型永久素材
 // http请求方式: POST，需使用https
 // https://api.weixin.qq.com/cgi-bin/material/add_material?access_token=ACCESS_TOKEN&type=TYPE
-// 调用示例（使用curl命令，用FORM表单方式新增一个其他类型的永久素材，curl命令的使用请自行查阅资料）
 func (m *Material) AddMaterial(filePath string, mediaType core.MediaType) core.Response {
 	log.Debug("Material|AddMaterial", filePath, mediaType)
 	if mediaType == core.MediaTypeVideo {
@@ -61,8 +60,6 @@ func (m *Material) AddMaterial(filePath string, mediaType core.MediaType) core.R
 //UploadVideo 新增其他类型永久素材
 // http请求方式: POST，需使用https
 // https://api.weixin.qq.com/cgi-bin/material/add_material?access_token=ACCESS_TOKEN&type=TYPE
-// 成功:
-// {"media_id":"HIWcj9t3AI_b8qCQSu8lrTBEyIAO-uPSQhTiI2uoENk"}
 func (m *Material) UploadVideo(filePath string, title, introduction string) core.Response {
 	log.Debug("Media|UploadVideo", filePath, title, introduction)
 	p := m.accessToken.GetToken().KeyMap()
@@ -83,10 +80,6 @@ func (m *Material) UploadVideo(filePath string, title, introduction string) core
 //Get 获取永久素材
 // http请求方式: POST,https协议
 // https://api.weixin.qq.com/cgi-bin/material/get_material?access_token=ACCESS_TOKEN
-// 失败:
-// {"errcode":40007,"errmsg":"invalid media_id hint: [YoxHSA07631538]"}
-// 成功:
-// {"title":"ceshi2","description":"only test","down_url":"http:\/\/203.205.158.71\/vweixinp.tc.qq.com\/1007_ad755ea12b3043e893e174d18de97f24.f10.mp4?vkey=22A7BCCDB429DF3613D50C1CAC510BDDCD12030895D782B3FAE00FB6989E4FFA640EB7EB8B498E560D08C84D808EF352BFFB0B15FA743556DB96BBF0239FC41F6DAFEEBA1024DBCA0954FBE09A66AA5381AB9CA50D1F8AE2&sha=0&save=1"}
 func (m *Material) Get(mediaID string) core.Response {
 	log.Debug("Material|Get", mediaID)
 	p := m.accessToken.GetToken().KeyMap()
@@ -102,10 +95,6 @@ func (m *Material) Get(mediaID string) core.Response {
 //Del 删除永久素材
 // http请求方式: POST
 // https://api.weixin.qq.com/cgi-bin/material/del_material?access_token=ACCESS_TOKEN
-// 成功:
-// {"errcode":0,"errmsg":"ok"}
-// 失败:
-// {"errcode":-1,"errmsg":"system error hint: [NX0zcA05993060]"}
 func (m *Material) Del(mediaID string) core.Response {
 	log.Debug("Material|Del", mediaID)
 	p := m.accessToken.GetToken().KeyMap()
