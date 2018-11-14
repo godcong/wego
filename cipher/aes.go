92c7b44779472c2f017ac2d9eaed8b3fcf8fcefe
godcong <jumbycc@163.com>
2018-11-14 15:25:56 +0800

    update cipher

--- cipher/aes.go
+++ cipher/aes.go
@@ -18,7 +18,39 @@ func NewDataCrypt(id string) *DataCrypt {
 	}
 }
 
+// DecryptAES128CBC ...
+func decryptAES128CBC(data, iv, key string) ([]byte, error) {
+	dKey, e := Base64Decode([]byte(key))
+	if e != nil {
+		return nil, e
+	}
+	dData, e := Base64Decode([]byte(data))
+	if e != nil {
+		return nil, e
+	}
+
+	dIv, e := Base64Decode([]byte(iv))
+	if e != nil {
+		return nil, e
+	}
+
+	block, e := aes.NewCipher(dKey)
+	if e != nil {
+		return nil, e
+	}
+
+	mode := cipher.NewCBCDecrypter(block, dIv)
+
+	mode.CryptBlocks(dData, dData)
+
+	//过滤所有 非正常字符结尾
+	idx := strings.LastIndex(string(dData), "}") + 1
+
+	return dData[:idx], nil
+}
+
 // Decrypt ...
+//Deprecated: change to cipher
 func (c *DataCrypt) Decrypt(data, iv, key string) ([]byte, error) {
 	dKey, e := Base64Decode([]byte(key))
 	if e != nil {
