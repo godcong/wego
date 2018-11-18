d14c905b4ee668f4c713902db968e730ce78ed2c
godcong <jumbycc@163.com>
2018-11-19 01:30:50 +0800

    update official account

---
+++ cipher/padding.go
@@ -0,0 +1,32 @@
+package cipher
+
+import "bytes"
+
+func ZeroPadding(ciphertext []byte, blockSize int) []byte {
+	padding := blockSize - len(ciphertext)%blockSize
+	padtext := bytes.Repeat([]byte{0}, padding)
+	return append(ciphertext, padtext...)
+}
+
+func ZeroUnPadding(origData []byte) []byte {
+	return bytes.TrimRightFunc(origData, func(r rune) bool {
+		return r == rune(0)
+	})
+}
+
+/*PKCS7Padding PKCS7Padding */
+func PKCS7Padding(ciphertext []byte, blockSize int) []byte {
+	padding := blockSize - len(ciphertext)%blockSize
+	padtext := bytes.Repeat([]byte{byte(padding)}, padding)
+	return append(ciphertext, padtext...)
+}
+
+/*PKCS7UnPadding PKCS7UnPadding */
+func PKCS7UnPadding(plantText []byte) []byte {
+	length := len(plantText)
+	unpadding := int(plantText[length-1])
+	if unpadding < 1 || unpadding > 32 {
+		unpadding = 0
+	}
+	return plantText[:(length - unpadding)]
+}
