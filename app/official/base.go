6b855fa941e7e471906f74ddd718bcc3b718e651
godcong <jumbycc@163.com>
2018-12-20 01:56:02 +0800

    change base to account

--- app/official/base.go
+++ app/official/base.go
@@ -5,31 +5,15 @@ import (
 	"github.com/godcong/wego/util"
 )
 
-/*Base 基本功能组件 */
-type Base struct {
-	*Account
-}
-
-func newBase(account *Account) *Base {
-	return &Base{
-		Account: account,
-	}
-}
-
-// NewBase 基础库
-func NewBase(config *core.Config) *Base {
-	return newBase(NewOfficialAccount(config))
-}
-
 /*ClearQuota 公众号的所有api调用（包括第三方帮其调用）次数进行清零
 HTTP请求方式:POST
 HTTP调用: https://api.weixin.qq.com/cgi-bin/clear_quota?access_token=ACCESS_TOKEN
 */
-func (b *Base) ClearQuota() core.Responder {
-	token := b.accessToken.GetToken()
+func (a *Account) ClearQuota() core.Responder {
+	token := a.accessToken.GetToken()
 
 	params := util.Map{
-		"appid": b.Get("app_id"),
+		"appid": a.Get("app_id"),
 	}
 	return core.PostJSON(Link(clearQuotaURLSuffix), token.KeyMap(), params)
 
@@ -39,7 +23,7 @@ func (b *Base) ClearQuota() core.Responder {
 HTTP请求方式: GET
 HTTP调用:https://api.weixin.qq.com/cgi-bin/getcallbackip?access_token=ACCESS_TOKEN
 */
-func (b *Base) GetCallbackIP() core.Responder {
-	token := b.accessToken.GetToken()
+func (a *Account) GetCallbackIP() core.Responder {
+	token := a.accessToken.GetToken()
 	return core.Get(Link(getCallbackIPURLSuffix), token.KeyMap())
 }
