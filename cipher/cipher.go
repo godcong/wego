0efb45d3d5d26999e2198f275337fb44e4db71bb
godcong <jumbycc@163.com>
2019-02-07 23:57:53 +0800

    update for wego-auth-manager

--- cipher/cipher.go
+++ cipher/cipher.go
@@ -1,5 +1,7 @@
 package cipher
 
+import "encoding/base64"
+
 // CryptType ...
 type CryptType int
 
@@ -25,20 +27,52 @@ var cipherList []InstanceFunc
 func init() {
 	cipherList = []InstanceFunc{
 		AES128CBC: NewAES128CBC,
-		//CryptAES128CBC,
+		AES256ECB: NewAES256ECB,
 		//CryptRSA,
 	}
 }
 
 // Option ...
 type Option struct {
-	IV    string
-	Key   string
-	Token string
-	AppID string
+	IV         string
+	Key        string
+	RSAPrivate string
+	RSAPublic  string
+	Token      string
+	AppID      string
 }
 
 // New create a new cipher
 func New(cryptType CryptType, option Option) Cipher {
 	return cipherList[cryptType](option)
+}
+
+func parseBytes(data interface{}) []byte {
+	switch tmp := data.(type) {
+	case []byte:
+		return tmp
+	case string:
+		return []byte(tmp)
+	default:
+		return nil
+	}
+}
+
+/*Base64Encode Base64Encode */
+func Base64Encode(b []byte) []byte {
+	buf := make([]byte, base64.StdEncoding.EncodedLen(len(b)))
+	base64.StdEncoding.Encode(buf, b)
+	return buf
+}
+
+/*Base64Decode Base64Decode */
+func Base64Decode(b []byte) ([]byte, error) {
+	buf := make([]byte, base64.StdEncoding.DecodedLen(len(b)))
+	n, err := base64.StdEncoding.Decode(buf, b)
+	return buf[:n], err
+}
+
+/*Base64DecodeString Base64DecodeString */
+func Base64DecodeString(s string) ([]byte, error) {
+	return base64.StdEncoding.DecodeString(s)
 }
