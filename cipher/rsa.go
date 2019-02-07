4e16e9c148e860ed2f2ade8e3d7350dae14e63bf
godcong <jumbycc@163.com>
2019-02-07 23:58:13 +0800

    update for wego-auth-manager

--- cipher/rsa.go
+++ cipher/rsa.go
@@ -8,9 +8,7 @@ import (
 	"encoding/base64"
 	"encoding/pem"
 	"errors"
-	"io/ioutil"
-
-	"github.com/godcong/wego/log"
+	"golang.org/x/exp/xerrors"
 )
 
 /* all defined errors */
@@ -23,51 +21,27 @@ var (
 )
 
 type cryptRSA struct {
-	KeyPath []byte
+	privateKey []byte
+	publicKey  []byte
+}
+
+// NewRSA ...
+func NewRSA(option Option) Cipher {
+	return &cryptRSA{
+		privateKey: []byte(option.RSAPrivate),
+		publicKey:  []byte(option.RSAPublic),
+	}
 }
 
 // Encrypt ...
-func (c *cryptRSA) Encrypt(interface{}) ([]byte, error) {
-	panic("implement me")
-}
-
-// Decrypt ...
-func (c *cryptRSA) Decrypt(interface{}) ([]byte, error) {
-	panic("implement me")
-}
-
-// Type ...
-func (*cryptRSA) Type() CryptType {
-	return RSA
-}
-
-// SetParameter ...
-func (c *cryptRSA) SetParameter(key string, val []byte) {
-	c.KeyPath = val
-}
-
-// GetParameter ...
-func (c *cryptRSA) GetParameter(key string) []byte {
-	return c.KeyPath
-}
-
-// Encrypt ...
-func (c *cryptRSA) Encrypt2(data []byte) ([]byte, error) {
-
-	publicKey, err := ioutil.ReadFile(string(c.KeyPath))
+func (c *cryptRSA) Encrypt(data interface{}) ([]byte, error) {
+	key, err := ParseRSAPublicKeyFromPEM(c.publicKey)
 	if err != nil {
-		log.Debug(err)
-		return nil, err
+		return nil, xerrors.Errorf("ParseRSAPublicKeyFromPEM:%w", err)
 	}
-	key, err := ParseRSAPublicKeyFromPEM(publicKey)
+	part, err := rsa.EncryptOAEP(sha1.New(), rand.Reader, key, parseBytes(data), nil)
 	if err != nil {
-		log.Debug(err)
-		return nil, err
-	}
-	part, err := rsa.EncryptOAEP(sha1.New(), rand.Reader, key, []byte(data), nil)
-	if err != nil {
-		log.Debug(err)
-		return nil, err
+		return nil, xerrors.Errorf("EncryptOAEP:%w", err)
 	}
 
 	buf := make([]byte, base64.StdEncoding.EncodedLen(len(part)))
@@ -76,18 +50,13 @@ func (c *cryptRSA) Encrypt2(data []byte) ([]byte, error) {
 }
 
 // Decrypt ...
-func (c *cryptRSA) Decrypt2(data []byte) ([]byte, error) {
-	privateKey, err := ioutil.ReadFile(string(c.KeyPath))
+func (c *cryptRSA) Decrypt(data interface{}) ([]byte, error) {
+	key, err := ParseRSAPrivateKeyFromPEM(c.privateKey)
 	if err != nil {
 		return nil, err
 	}
 
-	key, err := ParseRSAPrivateKeyFromPEM(privateKey)
-	if err != nil {
-		return nil, err
-	}
-
-	t, err := Base64Decode(data)
+	t, err := Base64Decode(parseBytes(data))
 	if err != nil {
 		return nil, err
 	}
@@ -99,9 +68,9 @@ func (c *cryptRSA) Decrypt2(data []byte) ([]byte, error) {
 	return b, nil
 }
 
-// CryptRSA ...
-func CryptRSA() Cipher {
-	return &cryptRSA{}
+// Type ...
+func (*cryptRSA) Type() CryptType {
+	return RSA
 }
 
 /*ParseRSAPrivateKeyFromPEM Parse PEM encoded PKCS1 or PKCS8 private key */
@@ -146,68 +115,4 @@ func ParseRSAPublicKeyFromPEM(key []byte) (*rsa.PublicKey, error) {
 
 	return nil, ErrorNotRSAPublicKey
 
-}
-
-/*RSADecrypt RSADecrypt */
-func RSADecrypt(pri string, text string) string {
-	privateKey, e := ioutil.ReadFile(pri)
-	if e != nil {
-		return ""
-	}
-
-	key, e := ParseRSAPrivateKeyFromPEM(privateKey)
-	if e != nil {
-		return ""
-	}
-
-	t, e := Base64Decode([]byte(text))
-	if e != nil {
-		return ""
-	}
-
-	b, e := rsa.DecryptOAEP(sha1.New(), rand.Reader, key, t, nil)
-	if e != nil {
-		return ""
-	}
-	return string(b)
-}
-
-/*RSAEncrypt RSAEncrypt */
-func RSAEncrypt(pub string, text string) string {
-	publicKey, e := ioutil.ReadFile(pub)
-	if e != nil {
-		log.Debug(e)
-		return ""
-	}
-	key, e := ParseRSAPublicKeyFromPEM(publicKey)
-	if e != nil {
-		log.Debug(e)
-		return ""
-	}
-	part, err := rsa.EncryptOAEP(sha1.New(), rand.Reader, key, []byte(text), nil)
-	if err != nil {
-		log.Debug(e)
-		return ""
-	}
-
-	return base64.StdEncoding.EncodeToString(part)
-}
-
-/*Base64Encode Base64Encode */
-func Base64Encode(b []byte) []byte {
-	buf := make([]byte, base64.StdEncoding.EncodedLen(len(b)))
-	base64.StdEncoding.Encode(buf, b)
-	return buf
-}
-
-/*Base64Decode Base64Decode */
-func Base64Decode(b []byte) ([]byte, error) {
-	buf := make([]byte, base64.StdEncoding.DecodedLen(len(b)))
-	n, err := base64.StdEncoding.Decode(buf, b)
-	return buf[:n], err
-}
-
-/*Base64DecodeString Base64DecodeString */
-func Base64DecodeString(s string) ([]byte, error) {
-	return base64.StdEncoding.DecodeString(s)
 }
