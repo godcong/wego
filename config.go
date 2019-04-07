770ebefd4db3efa08b2b7f548ca63523d9d44e5b
godcong <jumbycc@163.com>
2019-04-08 03:04:00 +0800

    update for wego-auth-manager

--- config.go
+++ config.go
@@ -7,13 +7,17 @@ import (
 
 // Config 配置文件，用来生成Property各种属性
 type Config struct {
-	AppID     string
-	AppSecret string
-	MchID     string
-	MchKey    string
-	PemCert   []byte
-	PemKEY    []byte
-	RootCA    []byte
+	AppID       string
+	AppSecret   string
+	MchID       string
+	MchKey      string
+	PemCert     []byte
+	PemKEY      []byte
+	RootCA      []byte
+	Token       string
+	AesKey      string
+	Scopes      []string
+	RedirectURI string
 }
 
 // DefaultConfig ...
