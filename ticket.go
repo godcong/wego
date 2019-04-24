c2e373a40f514bd6fbb5faae24fb6b0b7663acad
godcong <jumbycc@163.com>
2019-04-24 16:55:54 +0800

    update wego

--- ticket.go
+++ ticket.go
@@ -32,24 +32,30 @@ func NewTicket(accessToken *AccessToken) *Ticket {
 func (t *Ticket) Get(s string) Responder {
 	log.Debug("Ticket|Get", s)
 	p := t.KeyMap().Set("type", s)
-	return Get(util.URL(apiWeixin, ticketGetTicket), p)
+	return GetTicket(p)
 }
 
 // GetTicketRes ...
 func (t *Ticket) GetTicketRes(s string) (*TicketRes, error) {
 	var tr TicketRes
 	ticket := t.Get(s)
-	if e := ticket.Error(); e != nil {
-		return nil, e
+
+	if err := ticket.Error(); err != nil {
+		return nil, err
 	}
-	e := ticket.Unmarshal(&tr)
-	if e != nil {
-		return nil, e
+
+	if err := ticket.Unmarshal(&tr); err != nil {
+		return nil, err
 	}
+
 	return &tr, nil
 }
 
-// GetTicket ...
+//GetTicket 获取api_ticket
+// http请求方式: GET
+// https://api.weixin.qq.com/cgi-bin/ticket/getticket?access_token=ACCESS_TOKEN&type=wx_card
+// type: jsapi,wx_card
+// access_token: #token#
 func GetTicket(p util.Map) Responder {
 	return Get(util.URL(apiWeixin, ticketGetTicket), p)
 }
