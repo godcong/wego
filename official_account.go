4c5ac8be641edf94c5a2a3ace5ccd94e9ff45add
godcong <jumbycc@163.com>
2019-04-27 15:30:07 +0800

    update for wego-auth-manager

--- official_account.go
+++ official_account.go
@@ -488,12 +488,11 @@ func (obj *OfficialAccount) JSSDK() (*JSSDK, error) {
 	return obj.jssdk, nil
 }
 
-/*
-CommentOpen 打开文章评论
- https 请求方式: POST
- https://api.weixin.qq.com/cgi-bin/comment/open?access_token=ACCESS_TOKEN
- 失败:
-  {"errcode":88000,"errmsg":"without comment privilege"}
+/*CommentOpen 打开文章评论
+https 请求方式: POST
+https://api.weixin.qq.com/cgi-bin/comment/open?access_token=ACCESS_TOKEN
+失败:
+ {"errcode":88000,"errmsg":"without comment privilege"}
 */
 func (obj *OfficialAccount) CommentOpen(id, index int) Responder {
 	url := util.URL(obj.RemoteURL(), commentOpenURLSuffix)
@@ -503,17 +502,51 @@ func (obj *OfficialAccount) CommentOpen(id, index int) Responder {
 	})
 }
 
-/*
-CommentClose 关闭评论
- https 请求方式: POST
- https://api.weixin.qq.com/cgi-bin/comment/close?access_token=ACCESS_TOKEN
- 失败:
- {"errcode":88000,"errmsg":"without comment privilege"}
+/*CommentClose 关闭评论
+https 请求方式: POST
+https://api.weixin.qq.com/cgi-bin/comment/close?access_token=ACCESS_TOKEN
+失败:
+{"errcode":88000,"errmsg":"without comment privilege"}
 */
 func (obj *OfficialAccount) CommentClose(id, index int) Responder {
 	url := util.URL(obj.RemoteURL(), commentCloseURLSuffix)
 	return obj.Client().Post(context.Background(), url, nil, util.Map{
 		"msg_data_id": id,
 		"index":       index,
+	})
+}
+
+/*CommentList 获取文章评论
+https 请求方式: POST
+https://api.weixin.qq.com/cgi-bin/comment/list?access_token=ACCESS_TOKEN
+失败:
+{"errcode":88000,"errmsg":"without comment privilege"}
+*/
+func (obj *OfficialAccount) CommentList(id, index, begin, count, typ int) Responder {
+	url := util.URL(obj.RemoteURL(), commentListURLSuffix)
+	return obj.Client().Post(context.Background(), url, nil, util.Map{
+		"msg_data_id": id,
+		"index":       index,
+		"begin":       begin,
+		"count":       count,
+		"type":        typ,
+	})
+
+}
+
+/*CommentMarkElect  将评论标记精选
+https 请求方式: POST
+https://api.weixin.qq.com/cgi-bin/comment/markelect?access_token=ACCESS_TOKEN
+参数	是否必须	类型	说明
+id	是	int	群发返回的msg_data_id
+index	否	int	多图文时，用来指定第几篇图文，从0开始，不带默认操作该msg_data_id的第一篇图文
+user_comment_id	是	int	用户评论id
+*/
+func (obj *OfficialAccount) CommentMarkElect(id, index, userCommentID int) Responder {
+	url := util.URL(obj.RemoteURL(), commentMarkelectURLSuffix)
+	return obj.Client().Post(context.Background(), url, nil, util.Map{
+		"msg_data_id":     id,
+		"index":           index,
+		"user_comment_id": userCommentID,
 	})
 }
