2485d8bdae61f0f97ed5e6290987c169c8850cdc
godcong <jumbycc@163.com>
2019-05-18 22:20:43 +0800

    update for wego-auth-manager

--- official_account.go
+++ official_account.go
@@ -1352,13 +1352,53 @@ func (obj *OfficialAccount) QrCodeShow(ticket string) Responder {
 	return Get(u, util.Map{"ticket": url.QueryEscape(ticket)})
 }
 
-//Create 创建标签
-// http请求方式:POST（请使用https协议）
-// https://api.weixin.qq.com/cgi-bin/tags/create?access_token=ACCESS_TOKEN
-// 成功:
-// {"tag":{"id":100,"name":"testtag"}}
-func (obj *OfficialAccount) Create(name string) Responder {
-	log.Debug("Tag|Create", name)
-	u := util.URL(tagsCreate)
-	return obj.Client().Post(context.Background(), u, nil, util.Map{"tag": util.Map{"name": name}})
+//TemplateSetIndustry 设置所属行业
+// http请求方式: POST
+// https://api.weixin.qq.com/cgi-bin/template/api_set_industry?access_token=ACCESS_TOKEN
+//主行业	副行业	代码
+//IT科技	互联网/电子商务	1
+//IT科技	IT软件与服务	2
+//IT科技	IT硬件与设备	3
+//IT科技	电子技术	4
+//IT科技	通信与运营商	5
+//IT科技	网络游戏	6
+//金融业	银行	7
+//金融业	基金理财信托	8
+//金融业	保险	9
+//餐饮	餐饮	10
+//酒店旅游	酒店	11
+//酒店旅游	旅游	12
+//运输与仓储	快递	13
+//运输与仓储	物流	14
+//运输与仓储	仓储	15
+//教育	培训	16
+//教育	院校	17
+//政府与公共事业	学术科研	18
+//政府与公共事业	交警	19
+//政府与公共事业	博物馆	20
+//政府与公共事业	公共事业非盈利机构	21
+//医药护理	医药医疗	22
+//医药护理	护理美容	23
+//医药护理	保健与卫生	24
+//交通工具	汽车相关	25
+//交通工具	摩托车相关	26
+//交通工具	火车相关	27
+//交通工具	飞机相关	28
+//房地产	建筑	29
+//房地产	物业	30
+//消费品	消费品	31
+//商业服务	法律	32
+//商业服务	会展	33
+//商业服务	中介服务	34
+//商业服务	认证	35
+//商业服务	审计	36
+//文体娱乐	传媒	37
+//文体娱乐	体育	38
+//文体娱乐	娱乐休闲	39
+//印刷	印刷	40
+//其它	其它	41
+func (obj *OfficialAccount) TemplateSetIndustry(id1, id2 string) Responder {
+	log.Debug("OfficialAccount|TemplateSetIndustry", id1, id2)
+	u := util.URL(templateAPISetIndustry)
+	return obj.Client().Post(context.Background(), u, nil, util.Map{"industry_id1": id1, "industry_id2": id2})
 }
